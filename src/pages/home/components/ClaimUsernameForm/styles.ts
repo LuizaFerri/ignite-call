cbf641d5af0703905dac5510d3b3e280bbddace9
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-10-16 21:33:04 -0300

    Zod and react hook form

--- src/pages/home/components/ClaimUsernameForm/styles.ts
+++ src/pages/home/components/ClaimUsernameForm/styles.ts
@@ -1,12 +1,18 @@
-
-import { Box, styled } from '@ignite-ui/react'
+import { Box, styled, Text } from "@ignite-ui/react";
 export const Form = styled(Box, {
-  display: 'grid',
-  gridTemplateColumns: '1fr auto',
-  gap: '$2',
-  marginTop: '$4',
-  padding: '$4',
-  '@media(max-width: 600px)': {
-    gridTemplateColumns: '1fr',
+  display: "grid",
+  gridTemplateColumns: "1fr auto",
+  gap: "$2",
+  marginTop: "$4",
+  padding: "$4",
+  "@media(max-width: 600px)": {
+    gridTemplateColumns: "1fr",
   },
-})
+});
+
+export const FormAnnotation = styled(Text, {
+  marginTop: "$2",
+  [`> ${Text}`]: {
+    color: "$gray400",
+  },
+});
