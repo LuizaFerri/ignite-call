cd8d69830dc4df1eaffbb069c06edc778979ba48
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-10-17 22:17:42 -0300

    Set initial value for username

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -4,6 +4,8 @@ import { Container, Form, Header, FormError } from "./styles";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const registerFormSchema = z.object({
   username: z
@@ -23,13 +25,24 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   });
-  function handleRegister(data: RegisterFormData) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue("username", String(router.query.username));
+    }
+  }, [router.query?.username, setValue]);
+
+  async function handleRegister(data: RegisterFormData) {
     console.log(data);
   }
+
   return (
     <Container>
       <Header>
