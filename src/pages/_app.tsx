57e2519ecc8ee17ad2fb6a197a0451d1bb7f7137
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-10-21 19:55:28 -0300

    Set up Next Auth configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
-globalStyles()
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+globalStyles();
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
