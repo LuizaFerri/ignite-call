b9a97d9cf93c2e869590518360e4fbe187837cdd
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-10-30 00:06:56 -0300

    Implement profile update

--- src/pages/register/connect-calendar/index.tsx
+++ src/pages/register/connect-calendar/index.tsx
@@ -15,6 +15,10 @@ export default function ConnectCalendar() {
     await signIn("google", { callbackUrl: "/register/connect-calendar" });
   }
 
+  async function handleNextStep() {
+    await router.push("/register/time-intervals");
+  }
+
   return (
     <Container>
       <Header>
@@ -50,7 +54,7 @@ export default function ConnectCalendar() {
             permissões de acesso ao Google Calendar.
           </AuthError>
         )}
-        <Button type="submit" disabled={!isSignedIn}>
+        <Button type="submit" disabled={!isSignedIn} onClick={handleNextStep}>
           Próximo passo
           <ArrowRight />
         </Button>
